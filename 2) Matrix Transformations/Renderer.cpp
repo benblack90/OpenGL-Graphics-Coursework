6caa7fb62ab7482e3e47b71addb50d07aca9a9a2
benblack90 <b.schwarz3@ncl.ac.uk>
2023-10-24 15:32:22 +0100

    Add project files.

---
+++ 2) Matrix Transformations/Renderer.cpp
@@ -0,0 +1,63 @@
+#include "Renderer.h"
+
+Renderer::Renderer(Window& parent)
+	: OGLRenderer(parent)
+{
+	triangle = Mesh::GenerateTriangle();
+	matrixShader = new Shader("MatrixVertex.glsl", "colourFragment.glsl");
+
+	if (!matrixShader->LoadSuccess())
+	{
+		return;
+	}
+
+	init = true;
+	SwitchToOrthographic();
+}
+
+Renderer::~Renderer(void)
+{
+	delete triangle;
+	delete matrixShader;
+}
+
+void Renderer::SwitchToPerspective()
+{
+	projMatrix = Matrix4::Perspective(1.0f, 10000.0f, (float)width / (float)height, 45.0f);
+}
+
+void Renderer::SwitchToOrthographic()
+{
+	projMatrix = Matrix4::Orthographic(-1.0f, 10000.0f, width / 2.0f, -width / 2.0f, height / 2.0f, -height / 2.0f);
+}
+
+void Renderer::RenderScene()
+{
+	//clear the buffer to dark grey
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	BindShader(matrixShader);
+
+	//adjust fov value
+	if(projMatrix.values[11] == -1)
+	projMatrix = Matrix4::Perspective(1.0f, 10000.0f, (float)width / (float)height, fov);
+
+	//update the matrix uniform variable in the shader - first with the projection matrix, then the view matrix
+	glUniformMatrix4fv(glGetUniformLocation(matrixShader->GetProgram(), "projMatrix"), 1, false, projMatrix.values);
+	glUniformMatrix4fv(glGetUniformLocation(matrixShader->GetProgram(), "viewMatrix"), 1, false, viewMatrix.values);
+
+	//render 3 triangles offset in the x and y directions, and increasingly 'in the distance' in the z axis
+	for (int i = 0; i < 3; ++i)
+	{
+		Vector3 tempPos = position;
+		tempPos.z += (i * 500.0f);
+		tempPos.x -= (i * 100.0f);
+		tempPos.y -= (i * 100.0f);
+
+		//create transformation matrix by multiplying the constituent matrices together, and then apply that new transformation matrix
+		modelMatrix =   Matrix4::Translation(tempPos) * Matrix4::Scale(Vector3(scale, scale, scale)) * Matrix4::Rotation(rotation, Vector3(0, 1, 0));
+		glUniformMatrix4fv(glGetUniformLocation(matrixShader->GetProgram(), "modelMatrix"), 1, false, modelMatrix.values);
+
+		triangle->Draw();
+	}
+}
---
+++ Shaders/colourFragment.glsl
@@ -0,0 +1,11 @@
+#version 330 core
+
+in Vertex{
+ smooth vec4 colour;
+} IN;
+
+out vec4 fragColour;
+
+void main(void) {
+	fragColour = IN.colour;	
+}
